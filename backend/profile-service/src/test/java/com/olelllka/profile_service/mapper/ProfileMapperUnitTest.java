2e94ac4076cad00b36a5d5df6f5b8906fcb61d64
Oleshka <olehit32@gmail.com>
2025-01-04 22:28:59 +0100

    Added two more tests to mapper

---
+++ backend/profile-service/src/test/java/com/olelllka/profile_service/mapper/ProfileMapperUnitTest.java
@@ -0,0 +1,55 @@
+package com.olelllka.profile_service.mapper;
+
+import com.olelllka.profile_service.TestDataUtil;
+import com.olelllka.profile_service.domain.dto.ProfileDto;
+import com.olelllka.profile_service.domain.entity.ProfileEntity;
+import com.olelllka.profile_service.mapper.impl.ProfileMapper;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@Log
+public class ProfileMapperUnitTest {
+
+    @InjectMocks
+    private ProfileMapper mapper;
+
+    @Test
+    public void testThatMapperMapsToEntity() {
+        // given
+        ProfileDto dto = TestDataUtil.createNewProfileDto();
+        ProfileEntity entity = TestDataUtil.createNewProfileEntity();
+        // when
+        ProfileEntity result = mapper.toEntity(dto);
+        // then
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(result.getName(), entity.getName()) // basically it'll work with every field, it's not so complex
+        );
+    }
+
+    @Test
+    public void testThatMapperMapsToDto() {
+        // given
+        ProfileEntity entity = TestDataUtil.createNewProfileEntity();
+        ProfileEntity follow = TestDataUtil.createNewProfileEntity();
+        follow.setId(UUID.randomUUID());
+        entity.setFollowing(Set.of(follow));
+        // when
+        ProfileDto result = mapper.toDto(entity);
+        // then
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(result.getFollowing().getFirst(), follow.getId())
+        );
+    }
+
+}
