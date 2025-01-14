0d1bc0c5617dc97778b6a5c7fb77320b7ba1c64c
Oleshka <olehit32@gmail.com>
2025-01-14 07:59:25 +0100

    Added feed service, without functionality for now

---
+++ backend/feed-service/src/test/java/com/olelllka/feed_service/TestFeedServiceApplication.java
@@ -0,0 +1,11 @@
+package com.olelllka.feed_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FeedServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
