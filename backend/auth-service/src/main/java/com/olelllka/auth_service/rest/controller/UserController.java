7b62585fe7848a37ac1a1b98522cf6c6c3713eed
Oleshka <olehit32@gmail.com>
2025-02-21 14:19:02 +0100

    Did a change for the sake of ci

--- backend/auth-service/src/main/java/com/olelllka/auth_service/rest/controller/UserController.java
+++ backend/auth-service/src/main/java/com/olelllka/auth_service/rest/controller/UserController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
 
+
 @RestController
 @RequestMapping(path = "/auth")
 @RequiredArgsConstructor
