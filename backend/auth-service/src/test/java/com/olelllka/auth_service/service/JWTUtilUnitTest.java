ecda3582dfd915b8522596c0a753e77df611711a
Oleshka <olehit32@gmail.com>
2025-11-02 19:57:47 +0100

    Hotfix on auth unit test (jwt issue)

--- backend/auth-service/src/test/java/com/olelllka/auth_service/service/JWTUtilUnitTest.java
+++ backend/auth-service/src/test/java/com/olelllka/auth_service/service/JWTUtilUnitTest.java
@@ -1,9 +1,11 @@
 package com.olelllka.auth_service.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.UUID;
 
@@ -15,6 +17,11 @@ public class JWTUtilUnitTest {
 
     @InjectMocks
     private JWTUtil jwtUtil;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(jwtUtil, "key", "0d9aa86975f076cbb84ab112f361a4b254c6f553d41da0918b439300e592ed3f");
+    }
 
     @Test
     public void testThatJWTIsBeingGeneratedCorrectlyAndIsValid() {
