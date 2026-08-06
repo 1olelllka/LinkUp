a9f310b06d48ce55dad59e5b4ab373c4985cfbf5
Oleshka <olehit32@gmail.com>
2026-08-06 23:27:35 +0200

    Slightly modified tests on non-affected services

--- backend/feed-service/src/main/java/com/olelllka/feed_service/feign/ProfileInterface.java
+++ backend/feed-service/src/main/java/com/olelllka/feed_service/feign/ProfileInterface.java
@@ -15,7 +15,4 @@ public interface ProfileInterface {
 
     @GetMapping("/profiles/{profile_id}/followers")
     ResponseEntity<Page<ProfileDto>> getAllFollowersForProfile(@PathVariable UUID profile_id, Pageable pageable);
-
-    @GetMapping("/profiles/{profile_id}")
-    ResponseEntity<?> getProfileById(@PathVariable UUID profile_id);
 }
